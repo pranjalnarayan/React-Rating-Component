a5562efbecda9984d060a419912759349cb04f53
pranjalnarayan <pnarayan733@gmail.com>
2023-01-12 00:03:29 +0530

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import RatingContainer from './components/container/RatingContainer.jsx';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <RatingContainer />
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# React-Rating-Component
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-ysrfzb)
